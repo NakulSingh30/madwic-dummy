46133e615dd2c3b26841878b086086424e1472ca
NakulSingh30 <36731964+NakulSingh30@users.noreply.github.com>
2019-03-06 18:32:14 +0530

    v1

---
+++ jsFiles/animation.js
@@ -0,0 +1,17 @@
+// Wrap every letter in a span
+$('.heading').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.heading .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: function(el, i) {
+      return 500 + 100 * i;
+    }
+  });
