bf6421e6acad789bfebad151dd6040c28418cd18
NakulSingh30 <36731964+NakulSingh30@users.noreply.github.com>
2019-03-08 00:25:49 +0530

    Add files via upload

--- jsFiles/animation.js
+++ jsFiles/animation.js
@@ -1,17 +1,40 @@
 // Wrap every letter in a span
-$('.heading').each(function(){
-  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+
+$(document).ready(function(){
+  AOS.init();
+
+  $('.iphone-animation').addClass("play");
+
+   inputString = '.heading'
+   animateText(inputString);
+
+
+
+
 });
 
-anime.timeline({loop: false})
-  .add({
-    targets: '.heading .letter',
-    translateX: [40,0],
-    translateZ: 0,
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 1200,
-    delay: function(el, i) {
-      return 500 + 100 * i;
-    }
+
+
+function animateText(inputString){
+
+  $(inputString).each(function(){
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
   });
+
+  targetString = inputString + ' .letter';
+ 
+  
+  anime.timeline({loop: false})
+    .add({
+      targets: targetString,
+      translateX: [40,0],
+      translateZ: 0,
+      opacity: [0,1],
+      easing: "easeOutExpo",
+      duration: 1200,
+      delay: function(el, i) {
+        return 300 + 100 * i;
+      }
+    });
+
+}
